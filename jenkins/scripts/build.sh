1d6d5f6192a0e8013d011ddd37e84e13ca33342d
Charith <herath.charithjay@gmail.com>
2024-03-08 14:52:47 +0530

    Updated Jenkinsfile and added build script

---
+++ jenkins/scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DOCKERFILE=${WORKSPACE}/Dockerfile
+
+cat ${DOCKERFILE}
+docker build -t ${DOCKER_REPO}/userManagement -f ${DOCKERFILE} .
+  if [ $? -ne 0 ]
+  then
+    echo "docker build failed"
+    exit 1
+  fi
+
+docker tag ${DOCKER_REPO}/userManagement ${DOCKER_REPO}/userManagement:v${BUILD_NUMBER}
+docker push ${DOCKER_REPO}/userManagement:v${BUILD_NUMBER}
+docker rmi ${DOCKER_REPO}/userManagement
+docker rmi ${DOCKER_REPO}/userManagement:v${BUILD_NUMBER}
