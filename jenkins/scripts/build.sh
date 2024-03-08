c2cabe8c04451ccc377c701c7eeb5e2b0f7bdcfe
Charith <herath.charithjay@gmail.com>
2024-03-08 16:27:54 +0530

    Updated build script

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 DOCKERFILE=${WORKSPACE}/Dockerfile
+APP="usermanagement"
 
 cat ${DOCKERFILE}
-docker build -t ${DOCKER_REPO}/usermanagement -f ${DOCKERFILE} .
+docker build -t ${DOCKER_REPO}/${APP} -f ${DOCKERFILE} .
   if [ $? -ne 0 ]
   then
     echo "docker build failed"
     exit 1
   fi
 
-docker tag ${DOCKER_REPO}/userManagement ${DOCKER_REPO}/userManagement:v${BUILD_NUMBER}
-docker push ${DOCKER_REPO}/userManagement:v${BUILD_NUMBER}
-docker rmi ${DOCKER_REPO}/userManagement
-docker rmi ${DOCKER_REPO}/userManagement:v${BUILD_NUMBER}
+docker tag ${DOCKER_REPO}/${APP} ${DOCKER_REPO}/${APP}:v${BUILD_NUMBER}
+docker push ${DOCKER_REPO}/${APP}:v${BUILD_NUMBER}
+docker rmi ${DOCKER_REPO}/${APP}
+docker rmi ${DOCKER_REPO}/${APP}:v${BUILD_NUMBER}
